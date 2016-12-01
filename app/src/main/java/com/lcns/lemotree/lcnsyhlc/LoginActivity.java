9eacf941058fa0adc28d7a2cfb9803a17fea551a
lemotree <274229918@qq.com>
2016-12-01 12:19:16 +0800

    loginMain

--- app/src/main/java/com/lcns/lemotree/lcnsyhlc/LoginActivity.java
+++ app/src/main/java/com/lcns/lemotree/lcnsyhlc/LoginActivity.java
@@ -1,22 +1,44 @@
 package com.lcns.lemotree.lcnsyhlc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity implements android.view.View.OnClickListener{
 
     private Button mLoginBtn;
     private TextView mUserNameTv;
+    private  TextView mUserPwdTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initBody();
     }
 
     public void initBody(){
         mLoginBtn = (Button) findViewById(R.id.loginIdfirst);
+        mUserNameTv = (TextView) findViewById(R.id.edt_login_id);
+        mUserPwdTv = (TextView) findViewById(R.id.edt_login_pass);
+
+       mLoginBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view1) {
+        switch(view1.getId())
+        {
+            case R.id.loginIdfirst:
+                Intent in = new Intent();
+				in.setClass(LoginActivity.this,
+						MainActivity.class);
+				startActivity(in);
+				finish();
+        }
     }
 }
